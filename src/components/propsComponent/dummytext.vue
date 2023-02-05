dc728fcfb0e6018f422a9204bb94f77f4ed359b6
parthobepary <pallobbiswas326@gmail.com>
2023-02-05 23:17:43 +0600

    add props

--- src/components/propsComponent/dummytext.vue
+++ src/components/propsComponent/dummytext.vue
@@ -1,23 +1,29 @@
 <template>
-    <div class="container mx-auto text-center">
-        <h1>This is props component</h1>
-        <div>
-            <p>{{text}}</p>
-            <button @click="sendEmit" class="px-4 py-1 bg-blue-200">send emit</button>
-        </div>
+  <div class="container mx-auto text-center">
+    <h1>This is props component</h1>
+    <div>
+      <p>{{ text }}</p>
+      <button @click="sendEmit" class="px-4 py-1 bg-blue-200">send emit</button>
     </div>
+  </div>
 </template>
 
 <script setup>
-
-const {text} = defineProps(['text'])
-const emit = defineEmits(["sendData", "AgainSend"])
+const { text } = defineProps(["text"]);
+const emit = defineEmits(["sendData", "AgainSend"]);
 const sendEmit = () => {
-    emit('sendData', 'This is emit data')
-    emit('AgainSend', 'This is second emit data')
-}
+  emit("sendData", "This is emit data");
+  emit("AgainSend", "This is second emit data");
+};
+
+/* 
+const props = defineProps({
+  users: {
+    type: Array,
+    default: [],
+  },
+});
+*/
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
