6c3bbfcadcd796bc12a3aadf90fceb34dce601f4
beparypartho <pallobbiswas326@gmail.com>
2023-07-12 16:10:11 +0600

    add scroll nav

---
+++ src/layouts/nav.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+    <div class="text-3xl">
+        <div style="height: 100vh;">
+            <NuxtPage />
+        </div>
+        <commons-footer />
+    </div>
+</template>
---
+++ src/pages/scroll_nav.vue
@@ -0,0 +1,157 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+            </ul>
+        </nav>
+        <div>
+            <section id="home">
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eum neque dolore fugit tempora qui
+                        culpa ipsa, odit doloremque perspiciatis incidunt unde nam omnis minus corporis, voluptas eaque
+                        impedit. Et neque veniam ex nam praesentium iste? Velit animi eligendi dolorem eveniet esse maiores!
+                        Omnis aliquid quis dolorem non qui ut corporis deleniti modi voluptatum quaerat vero aut consequatur
+                        amet rerum consequuntur expedita, recusandae ipsa minus ad distinctio placeat unde aliquam tempore
+                        dicta? Dicta commodi ducimus, nemo iure voluptates nobis molestiae velit eum explicabo harum, fuga
+                        voluptas laborum. Aut pariatur aperiam cum perspiciatis ipsam magnam excepturi ad consequatur, totam
+                        laudantium accusamus eligendi assumenda eum aspernatur saepe culpa cumque hic debitis ut qui vel
+                        delectus quis quos. Facere suscipit soluta dolorem vero fugiat temporibus, corporis esse aperiam,
+                        iste culpa ipsum aspernatur nostrum a assumenda. Deserunt illum consectetur dolores consequuntur rem
+                        et quod dolore animi autem, alias laboriosam laudantium pariatur. Consequuntur, magnam
+                        necessitatibus! Accusantium commodi tempora, quod molestiae libero doloremque hic, minus voluptate,
+                        corrupti dolorem debitis mollitia facilis consequuntur obcaecati culpa dolores veniam optio a fuga
+                        modi aliquid! Nemo eligendi, at, modi veniam, mollitia iure non tempore in rerum deleniti
+                        praesentium consectetur hic dicta alias corrupti deserunt consequatur? Optio nostrum nam saepe aut.
+                    </p>
+                </div>
+            </section>
+
+            <section id="about">
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eum neque dolore fugit tempora qui
+                        culpa ipsa, odit doloremque perspiciatis incidunt unde nam omnis minus corporis, voluptas eaque
+                        impedit. Et neque veniam ex nam praesentium iste? Velit animi eligendi dolorem eveniet esse maiores!
+                        Omnis aliquid quis dolorem non qui ut corporis deleniti modi voluptatum quaerat vero aut consequatur
+                        amet rerum consequuntur expedita, recusandae ipsa minus ad distinctio placeat unde aliquam tempore
+                        dicta? Dicta commodi ducimus, nemo iure voluptates nobis molestiae velit eum explicabo harum, fuga
+                        voluptas laborum. Aut pariatur aperiam cum perspiciatis ipsam magnam excepturi ad consequatur, totam
+                        laudantium accusamus eligendi assumenda eum aspernatur saepe culpa cumque hic debitis ut qui vel
+                        delectus quis quos. Facere suscipit soluta dolorem vero fugiat temporibus, corporis esse aperiam,
+                        iste culpa ipsum aspernatur nostrum a assumenda. Deserunt illum consectetur dolores consequuntur rem
+                        et quod dolore animi autem, alias laboriosam laudantium pariatur. Consequuntur, magnam
+                        necessitatibus! Accusantium commodi tempora, quod molestiae libero doloremque hic, minus voluptate,
+                        corrupti dolorem debitis mollitia facilis consequuntur obcaecati culpa dolores veniam optio a fuga
+                        modi aliquid! Nemo eligendi, at, modi veniam, mollitia iure non tempore in rerum deleniti
+                        praesentium consectetur hic dicta alias corrupti deserunt consequatur? Optio nostrum nam saepe aut.
+                    </p>
+                </div>
+            </section>
+
+            <section id="contact">
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eum neque dolore fugit tempora qui
+                        culpa ipsa, odit doloremque perspiciatis incidunt unde nam omnis minus corporis, voluptas eaque
+                        impedit. Et neque veniam ex nam praesentium iste? Velit animi eligendi dolorem eveniet esse maiores!
+                        Omnis aliquid quis dolorem non qui ut corporis deleniti modi voluptatum quaerat vero aut consequatur
+                        amet rerum consequuntur expedita, recusandae ipsa minus ad distinctio placeat unde aliquam tempore
+                        dicta? Dicta commodi ducimus, nemo iure voluptates nobis molestiae velit eum explicabo harum, fuga
+                        voluptas laborum. Aut pariatur aperiam cum perspiciatis ipsam magnam excepturi ad consequatur, totam
+                        laudantium accusamus eligendi assumenda eum aspernatur saepe culpa cumque hic debitis ut qui vel
+                        delectus quis quos. Facere suscipit soluta dolorem vero fugiat temporibus, corporis esse aperiam,
+                        iste culpa ipsum aspernatur nostrum a assumenda. Deserunt illum consectetur dolores consequuntur rem
+                        et quod dolore animi autem, alias laboriosam laudantium pariatur. Consequuntur, magnam
+                        necessitatibus! Accusantium commodi tempora, quod molestiae libero doloremque hic, minus voluptate,
+                        corrupti dolorem debitis mollitia facilis consequuntur obcaecati culpa dolores veniam optio a fuga
+                        modi aliquid! Nemo eligendi, at, modi veniam, mollitia iure non tempore in rerum deleniti
+                        praesentium consectetur hic dicta alias corrupti deserunt consequatur? Optio nostrum nam saepe aut.
+                    </p>
+                </div>
+            </section>
+
+            <section id="portfolio">
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eum neque dolore fugit tempora qui
+                        culpa ipsa, odit doloremque perspiciatis incidunt unde nam omnis minus corporis, voluptas eaque
+                        impedit. Et neque veniam ex nam praesentium iste? Velit animi eligendi dolorem eveniet esse maiores!
+                        Omnis aliquid quis dolorem non qui ut corporis deleniti modi voluptatum quaerat vero aut consequatur
+                        amet rerum consequuntur expedita, recusandae ipsa minus ad distinctio placeat unde aliquam tempore
+                        dicta? Dicta commodi ducimus, nemo iure voluptates nobis molestiae velit eum explicabo harum, fuga
+                        voluptas laborum. Aut pariatur aperiam cum perspiciatis ipsam magnam excepturi ad consequatur, totam
+                        laudantium accusamus eligendi assumenda eum aspernatur saepe culpa cumque hic debitis ut qui vel
+                        delectus quis quos. Facere suscipit soluta dolorem vero fugiat temporibus, corporis esse aperiam,
+                        iste culpa ipsum aspernatur nostrum a assumenda. Deserunt illum consectetur dolores consequuntur rem
+                        et quod dolore animi autem, alias laboriosam laudantium pariatur. Consequuntur, magnam
+                        necessitatibus! Accusantium commodi tempora, quod molestiae libero doloremque hic, minus voluptate,
+                        corrupti dolorem debitis mollitia facilis consequuntur obcaecati culpa dolores veniam optio a fuga
+                        modi aliquid! Nemo eligendi, at, modi veniam, mollitia iure non tempore in rerum deleniti
+                        praesentium consectetur hic dicta alias corrupti deserunt consequatur? Optio nostrum nam saepe aut.
+                    </p>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+<script setup>
+definePageMeta({
+    layout: 'nav'
+})
+window.addEventListener('scroll', function () {
+    var sections = document.querySelectorAll('section');
+    var navLinks = document.querySelectorAll('nav a');
+
+    var currentSection = '';
+
+    sections.forEach(function (section) {
+        var sectionTop = section.offsetTop;
+        var sectionHeight = section.offsetHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 2) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(function (link) {
+        link.classList.remove('active');
+        if (link.getAttribute('href').substring(1) === currentSection) {
+            link.classList.add('active');
+        }
+    });
+});
+</script>
+<style>
+nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: #f1f1f1;
+}
+
+nav ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+nav li {
+    margin: 0 10px;
+}
+
+nav a {
+    text-decoration: none;
+    color: #333;
+}
+
+nav a.active {
+    font-weight: bold;
+}
+
+section {
+    height: 100vh;
+    margin-top: 50px;
+    /* To offset the fixed navigation */
+}</style>
